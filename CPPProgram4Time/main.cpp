d9ced67fb7aec2acdb84c9b03a45158e07fc43bb
Vincent Fiore <vinnyfiore@vincents-air-2.home>
2014-04-15 16:20:50 -0400

    Initial Commit

---
+++ CPPProgram4Time/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CPPProgram4Time
+//
+//  Created by Vincent Fiore on 4/15/14.
+//  Copyright (c) 2014 Vincent Fiore. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
